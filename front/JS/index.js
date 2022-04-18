a29b827be9c2bdc1141d5b69c725e257f23c4d20
Arnaud293 <arnaudlt293@gmail.com>
2022-04-18 13:12:40 +0200

    Display product homepage

--- front/JS/index.js
+++ front/JS/index.js
@@ -1,8 +1,41 @@
+const allproducts = document.getElementById('items');
+const productscards = document.querySelector('article');
 
-document.querySelector('#items')
-let url = `http://localhost:3000/api/products`;
+let canapData = []
 
-fetch(url).then(response => response.json().then(data => console.log(data))
-);
+// Récupérer les donnée des canapés
+
+const getallproducts = async () => {
+    await fetch('http://localhost:3000/api/products')
+        .then((res) => res.json())
+        .then((data) => canapData = data);
+
+    console.log(canapData);
+    allproductsdisplay()
+
+}
+getallproducts();
+
+// Afficher les canapés sur la page d'accueil
+
+const allproductsdisplay = () => {
+
+    allproducts.innerHTML = canapData.map((canap) =>
 
 
+        `
+        <a href="product.html">
+            <article>
+                <img src="${canap.imageUrl}">
+                 <h3 class="productName">${canap.name}</h3>
+                 <p class="productDescription">${canap.description}</p>
+             </article>
+                
+                
+        </a>
+        `
+
+    )
+        .join("");
+}
+allproductsdisplay();
