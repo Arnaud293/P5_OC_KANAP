1cb3caa3c4982a81ae1053c2b86dc24f6a418bc7
Arnaud293 <arnaudlt293@gmail.com>
2022-04-22 19:12:26 +0200

    70% done at localstorage from product to basket

--- front/JS/product.js
+++ front/JS/product.js
@@ -27,12 +27,16 @@ const DisplayById = async () => {
 
     let productImg = document.querySelector('.item__img');
     productImg.innerHTML = `<img src="${canapData.imageUrl}" alt="${canapData.altTxt}">`;
+
     let productTitle = document.getElementById('title');
     productTitle.innerText = canapData.name;
+
     let productPrice = document.getElementById('price');
     productPrice.innerText = canapData.price;
+
     let productDescription = document.getElementById('description');
     productDescription.innerHTML = canapData.description;
+
     // Mapping pour choix de la couleur (plusieurs éléments)
     document.querySelector("#colors").insertAdjacentHTML
         ("beforeend", canapData.colors.map(colors => `<option value="${colors}">${colors}</option>`)
@@ -40,6 +44,97 @@ const DisplayById = async () => {
 
 
 
+
 }
 
 DisplayById()
+
+// Ecoute du panier
+
+// ------------- Fonction if pour choix produit mise de côté --------------
+
+// if (Number(quantity) <= 0 && color == '') {
+//     alert('Veuillez choisir une couleur et une quantité');
+//     return
+// }
+// ------------------------------------------------------------------------
+
+
+
+
+
+document.querySelector('#addToCart').addEventListener('click', function () {
+
+
+
+
+
+    // Récupération des options du produits : 
+
+    let optionProduct = {
+        name: canapData.name,
+        id: canapId,
+        color: document.getElementById('colors').value,
+        quantity: document.getElementById('quantity').value,
+
+    }
+
+    // Aucun produit n'est sélectionné
+
+    if (Number(optionProduct.quantity) <= 0 && (optionProduct.color) == '') {
+        alert('Veuillez choisir une couleur et une quantité');
+        return
+    }
+
+    // Confirmation d'ajout au panier 
+
+    const popupConfirmation = () => {
+        if (window.confirm(`${optionProduct.name} ${optionProduct.color} a été ajouté à votre panier :
+OK pour consulter votre panier, ANNULER pour continuer vos achats`)) {
+            window.location.href = 'cart.html';
+
+        } else {
+            window.location.href = 'index.html';
+
+        }
+    };
+
+    let notEmpty = JSON.parse(localStorage.getItem("product"));
+
+
+    // Produit(s) présent(s) dans le localstorage
+
+    if (notEmpty) {
+        notEmpty.push(optionProduct);
+        let sameId = notEmpty.find(p => p.id == product.id && p.color == product.color);
+        if (sameId != undefined) {
+            sameId.quantity++;
+        } else {
+            product.quantity = 1;
+            notEmpty.push(optionProduct);
+
+        }
+        localStorage.setItem("product", JSON.stringify(notEmpty));
+
+        popupConfirmation();
+
+
+
+    }
+
+
+    // Pas de produit(s) dans le local storage 
+
+    else {
+        notEmpty = [];
+        notEmpty.push(optionProduct);
+        localStorage.setItem("product", JSON.stringify(notEmpty));
+        popupConfirmation();
+    }
+
+
+}
+)
+
+
+
