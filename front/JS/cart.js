bfad1251a972b9974b65c4c09f1d4d983f5ee983
Arnaud293 <arnaudlt293@gmail.com>
2022-04-23 13:27:55 +0200

    starting to write function display in bskt, trying to use more apendchildE

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -2,3 +2,18 @@
 
 let notEmpty = JSON.parse(localStorage.getItem("product"));
 console.log(notEmpty);
+
+// Afficher les produits présents dans le panier 
+
+const displayCart = () => {
+
+    for (let product in notEmpty) {
+        let cartArticle = document.createElement("article");
+        document.getElementById('cart__items').appendChild(cartArticle);
+        cartArticle.className = "cart__item";
+
+
+    }
+}
+
+displayCart()
