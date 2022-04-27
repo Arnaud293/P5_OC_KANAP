c35c90d84d086f2b53a8d7077aca008640c98023
Arnaud293 <arnaudlt293@gmail.com>
2022-04-27 20:05:58 +0200

    fetch + LS by ID is ok, go to display all informations in basket

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -9,55 +9,60 @@ let itemInCart = JSON.parse(localStorage.getItem("product"));
 console.table(itemInCart);
 
 
+
 //  Récupérer les donnée des canapés hors lS 
 
 let newArray;
 
-const getCanapData = async (newArray = []) => {
+const getCanapData = async () => {
 
     const res = await fetch(`http://localhost:3000/api/products`);
     canapData = await res.json();
 
 
-    // Revoir cette fonction et bien la comprendre !
+    // Si l'id est le même dans canapData et le localStorage
 
-
+    newArray = [];
     for (id of canapData) {
-        if (itemInCart.some(lsId => {
+        if (!itemInCart) {
+            document.getElementById('cart__items').insertAdjacentHTML('beforeend', `<p>Votre panier est vide.</p>`);
+            document.getElementById('cart__items').style.textAlign = "center";
+            return
+        }
+        else if (itemInCart.some(lsId => {
+
             return lsId.id === id._id
+
+
         })) {
             newArray.push(id);
+            console.table(id);
+            displayBasket()
         }
     }
 
-
-
 }
-getCanapData();
-console.log(newArray)
-
+getCanapData()
 
 
 // Si le panier est vide
+const displayBasket = () => {
 
-if (!itemInCart) {
-    document.getElementById('cart__items').insertAdjacentHTML('beforeend', `<p>Votre panier est vide.</p>`);
-    document.getElementById('cart__items').style.textAlign = "center";
+    if (!itemInCart) {
+        document.getElementById('cart__items').insertAdjacentHTML('beforeend', `<p>Votre panier est vide.</p>`);
+        document.getElementById('cart__items').style.textAlign = "center";
+        return
+    }
 
-}
+    //  Sinon afficher l'article, + autant d'article que d'éléments dans le LS
 
-//  Sinon afficher l'article, + autant d'article que d'éléments dans le LS
-
-else {
-
-    for (let p = 0; p < itemInCart.length; p++) {
-
+    else {
 
         let cartArticle = document.createElement("article");
         document.getElementById('cart__items').appendChild(cartArticle);
         cartArticle.className = "cart__item";
-        cartArticle.setAttribute('data-id', itemInCart[p].id);
-        cartArticle.setAttribute('data-color', itemInCart[p].color);
+        cartArticle.setAttribute('data-id', itemInCart.id);
+        cartArticle.setAttribute('data-color', itemInCart.color);
 
 
         // IMG
@@ -67,17 +72,12 @@ else {
         divCartImg.className = "cart__item__content";
         let cartImg = document.createElement("img");
         divCartImg.appendChild(cartImg);
-
-
-
-
-
-
-
+        cartImg.src = id.imageUrl;
+        cartImg.alt = id.altTxt;
 
     }
-
 }
+
 
 
 
